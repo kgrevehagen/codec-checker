0455a52e8a15d669fa856ef893672baee952cd49
Kristian Greve Hagen <kristian.greve.hagen@gmail.com>
2025-02-03 23:32:24 +0100

    Update dependencies etc so it compiles and runs in 2025

--- app/src/main/java/com/kgrevehagen/android/codecchecker/CodecHelper.kt
+++ app/src/main/java/com/kgrevehagen/android/codecchecker/CodecHelper.kt
@@ -1,9 +1,11 @@
 package com.kgrevehagen.android.codecchecker
 
-import com.google.android.exoplayer2.mediacodec.MediaCodecUtil
-import com.google.android.exoplayer2.util.MimeTypes
-import java.util.Collections
+import androidx.annotation.OptIn
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.mediacodec.MediaCodecUtil
 
+@OptIn(UnstableApi::class)
 object CodecHelper {
 
     fun hasAC4DecodingCapabilities() =
@@ -26,31 +28,4 @@ object CodecHelper {
 
     fun getRawDecodingCapabilities() =
         MediaCodecUtil.getDecoderInfos(MimeTypes.AUDIO_RAW, false, false)
-
-
-//    fun yo(mimeType: String, secure: Boolean, tunneling: Boolean){
-//            val key = CodecKey(mimeType, secure, tunneling)
-//            var mediaCodecList = if (Util.SDK_INT >= 21)
-//                MediaCodecListCompatV21(secure, tunneling)
-//            else
-//                MediaCodecListCompatV16()
-//            var decoderInfos = getDecoderInfosInternal(key, mediaCodecList)
-//            if (secure && decoderInfos.isEmpty() && 21 <= Util.SDK_INT && Util.SDK_INT <= 23) {
-//                // Some devices don't list secure decoders on API level 21 [Internal: b/18678462]. Try the
-//                // legacy path. We also try this path on API levels 22 and 23 as a defensive measure.
-//                mediaCodecList = MediaCodecListCompatV16()
-//                decoderInfos = getDecoderInfosInternal(key, mediaCodecList)
-//                if (!decoderInfos.isEmpty()) {
-//                    Log.w(
-//                        TAG, "MediaCodecList API didn't list secure decoder for: " + mimeType
-//                                + ". Assuming: " + decoderInfos.get(0).name
-//                    )
-//                }
-//            }
-//            applyWorkarounds(mimeType, decoderInfos)
-//            val unmodifiableDecoderInfos =
-//                Collections.unmodifiableList<MediaCodecInfo>(decoderInfos)
-//            decoderInfosCache.put(key, unmodifiableDecoderInfos)
-//            return unmodifiableDecoderInfos
-//    }
 }
